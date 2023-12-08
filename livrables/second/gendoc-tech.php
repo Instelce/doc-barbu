9b9a136c40d37aa7acc747da55be265bd005fe13
cebesson <cebesson@etudiant.univ-rennes1.fr>
2023-12-08 16:38:21 +0100

    ajout de la commande --onefile et --main dans le script gendoc-tech.php

--- livrables/second/gendoc-tech.php
+++ livrables/second/gendoc-tech.php
@@ -11,11 +11,11 @@
         ],
     ];
 
-
     // plusieurs commandes
     // --dir <dirname>              Cherche tous les fichiers présents dans le dossier et génère leur documentation.
     // --main <main_program_file>   Génère la documentation du fichier principal et des fichiers importé
     // --onefile <file_name>        Génère la documentation d'un fichier
+    // --help                       Donne la documentation des commandes
 
     $commands = ["--dir", "--main", "--onefile"];
     $files = [];
@@ -29,14 +29,49 @@
         } else if ($commandValue == '') {
             exit("Veuillez saisir la valeur de la commande.");
         } else {
-            echo "Génération de la documentation...\n";
-
             if ($command == "--dir") {
                 $files = glob(getcwd() . "\\" . $commandValue . "\\*.c");
 
                 echo getcwd() . "\n";
                 print_r($files);
             }
+
+            if (file_exists($commandValue)) {
+                if ($command == "--onefile") {
+                    array_push($files, $commandValue);
+                }
+
+                if ($command == "--main") {
+                    $path = explode("\\", $commandValue);
+                    array_pop($path);
+                    $path = join("\\", $path);
+                    echo "\n" . $path . "\n";
+
+                    array_push($files, $commandValue);
+
+                    // récupère les includes
+                    $includePattern = '/#include\s+"([^"]*)"/m';
+                    preg_match_all($includePattern, file_get_contents($commandValue), $includeMatches);
+
+                    foreach ($includeMatches[0] as $include) {
+                        // echo str_replace("\"", "", explode(" ", $include)[1]) . "\n";
+                        $headerFileName = str_replace("\"", "", explode(" ", $include)[1]);
+                        $cFileName = str_replace("h", "c", $headerFileName);
+
+                        array_push($files, $path . "\\" . $headerFileName);
+
+                        if (file_exists($path . "\\" . $cFileName)) {
+                            array_push($files, $path . "\\" . $cFileName);
+                        }
+                    }
+                    print_r($files);
+                }
+            } else {
+                exit("Le fichier n'existe pas !");
+            }
+
+
+            echo "Génération de la documentation...\n";
         }
     } else {
         exit("Aucune commande trouvé.");
@@ -47,8 +82,9 @@
 
     // initialisation des données
     foreach ($files as $path) {
+        $lastIndex = count(explode("/", $path)) - 1;
         array_push($data, [
-            "name" => explode("/", $path)[2],
+            "name" => explode("/", $path)[$lastIndex],
             "path" => $path,
             "contents" => [
                 "auteur" => "",
