a701b03660cfc5717b79f8675dc02e954e1defa6
yaitouazzou <yaitouazzou@etudiant.univ-rennes1.fr>
2024-01-10 16:29:28 +0100

    optimisation et correction de l automatisation

--- livrables/second/sae103.bash
+++ livrables/second/sae103.bash
@@ -7,8 +7,11 @@ NOM_DU_VOLUME="sae103"
 CHEMIN_LOCAL="/work"
 
 # Images Docker à utiliser
-IMAGE_DOCKER_CLOCK="clock"
-IMAGE_DOCKER_PDF=" sae103-html2pdf"
+IMAGE_DOCKER_INFINI="clock"
+IMAGE_DOCKER_PDF="sae103-html2pdf"
+
+# Nom du dossier final
+NOM_DOSSIER="dossier_sae103"
 
 # Nom du conteneur Docker
 CONTENEUR_INFINI="sae103-forever"
@@ -19,13 +22,13 @@ docker volume create $NOM_DU_VOLUME
 echo "création de volume"
 
 # Lancer le conteneur en montant le volume
-docker image pull $IMAGE_DOCKER_CLOCK
-docker image pull $IMAGE_DOCKER_PDF
-docker run -v $NOM_DU_VOLUME:$CHEMIN_LOCAL $IMAGE_DOCKER_CLOCK -tid --name $CONTENEUR_INFINI
+docker image pull latest $IMAGE_DOCKER_INFINI
+docker image pull latest $IMAGE_DOCKER_PDF
+docker run --rm -v $NOM_DU_VOLUME:$CHEMIN_LOCAL $IMAGE_DOCKER_INFINI -tid --name $CONTENEUR_INFINI
 echo "lancement des conteneurs"
 
 # Copie des fichiers c vers dans le volume avec le conteneur sae103-forever comme cible
-docker cp *c $CONTENEUR_INFINI:$CHEMIN_LOCAL
+docker cp *.c $CONTENEUR_INFINI:$CHEMIN_LOCAL
 echo "copie des fichiers c faite"
 
 # Execution des traitements dans le conteneur sae103-forever puis génération des fichiers pdf grâce à html2pdf
@@ -35,18 +38,17 @@ docker run -v $NOM_DU_VOLUME:$CHEMIN_LOCAL $IMAGE_DOCKER_PDF html2pdf DOC_UTILIS
 echo "création des fichiers html et pdf"
 
 # Création du dossier où seront déplacées les documentations format PDF puis de l'archive en format tar.gz
-docker exec $CONTENEUR_INFINI sh "dossier_sae103"
-docker exec $CONTENEUR_INFINI sh "mv *.html *.c *.pdf dossier_sae103"
-docker exec $CONTENEUR_INFINI sh "tar czvf dossier_sae103.tar.gz dossier_sae103"
+docker exec $CONTENEUR_INFINI sh "$NOM_DOSSIER"
+docker exec $CONTENEUR_INFINI sh "mv *.html *.c *.pdf $NOM_DOSSIER"
+docker exec $CONTENEUR_INFINI sh "tar czvf $NOM_DOSSIER.tar.gz $NOM_DOSSIER"
 echo "création du fichier tgz contenant l archive final"
 
 # Récupération de l'archive contenant les documentations en format PDF
-docker cp $CONTENEUR_INFINI:$CHEMIN_LOCAL/dossier_sae103 .
+docker cp $CONTENEUR_INFINI:$CHEMIN_LOCAL/$NOM_DOSSIER .
 echo "copie du fichier tgz vers le repertoire courant"
 
 # Suppression du conteneur et du volume
 docker kill $CONTENEUR_INFINI
-docker rm $CONTENEUR_INFINI $CONTENEUR_PDF
 docker image prune
 docker volume rm $NOM_DU_VOLUME
 echo "supression des images, des conteneurs et du volume"
